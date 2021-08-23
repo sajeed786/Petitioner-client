83649aa41d75f12ff99c95206668fb1d4e837c02
Sk Sajeed Hossain <hossains018@gmail.com>
2021-08-24 01:40:38 +0530

    Modified Homepage component

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const apiUrl = 'https://petitioner-server.herokuapp.com/';
+const API = axios.create({ baseURL: 'http://localhost:8000' });
 
 API.interceptors.request.use((req) => {
     if(localStorage.getItem('profile')) {
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,12 +1,20 @@
 import React from 'react'
-//import { Link } from 'react-router-dom';
-//import Navbar from '../Navbar/Navbar';
+
+import './styles.css'
 
 const Home = () => {
 
     return (
-        <div>
-            <button style={{textAlign: 'center'}}><a href="/start-a-petition"> Start A Petition </a></button>
+        <div className="banner-area">
+            <div className="content-area">
+            {/* <div className="content"> */}
+                <h1>Voice for a change</h1>
+                
+                <p>Victories everyday</p>
+                
+                <a className="petition-btn" href="/start-a-petition"> Start A Petition </a>
+            {/* </div> */}
+            </div>
         </div>
     )
 }
